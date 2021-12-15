1be6cb16bf492b550b37cdc0f9537a857e1b5eb7
Joe Musashi <63985175+JoeMusashi9999@users.noreply.github.com>
2021-12-15 09:33:16 -0800

    Update Game.js

--- Game.js
+++ Game.js
@@ -1,2 +1,12 @@
 #editing in progress
 console.out("welcome to crappy clicker! have fun?")
+#acheivement setting
+acheivement1 = false
+acheivement2 = false
+acheivement3 = false
+acheivement4 = false
+acheivement5 = false
+acheivement6 = false
+acheivement7 = false
+acheivement8 = false
+acheivement9 = false
