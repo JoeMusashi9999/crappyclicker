a282b76f6744511f0ebc6729ff3edc7ca72f6f6a
Joe Musashi <63985175+JoeMusashi9999@users.noreply.github.com>
2021-12-16 11:05:53 -0800

    Update Game.js

--- Game.js
+++ Game.js
@@ -1,5 +1,8 @@
 #editing in progress
 console.out("welcome to crappy clicker! have fun?")
+
+
+
 #acheivement setting
 acheivement1 = false
 acheivement2 = false
@@ -11,8 +14,20 @@ acheivement7 = false
 acheivement8 = false
 acheivement9 = false
 
+function checkAcheivemnts(): (
+  if crap == 100: (
+     acheivement1 = true
+     console.out("you got the acheivemnt 100 crap!")
+  )
 
 
+
+
+
+crap = 0
+crapperclick = 1
 crappersec = 0
 if button.click() = true: (
-  
+  crap = crap ++ crapperclick
+ )
+
