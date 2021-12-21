73b15f0563d871fe4ba89cb7f4492629fb6f3f42
Joe Musashi <63985175+JoeMusashi9999@users.noreply.github.com>
2021-12-20 20:42:26 -0800

    Create acheivements.js

---
+++ javascript scripts/acheivements.js
@@ -0,0 +1,34 @@
+//editing in progress
+
+  
+//acheivement setting
+var acheivement1 = false;
+var acheivement2 = false;
+var acheivement3 = false;
+var acheivement4 = false;
+var acheivement5 = false;
+var acheivement6 = false;
+var acheivement7 = false;
+var acheivement8 = false;
+var acheivement9 = false;
+
+    
+
+
+
+
+var crap = 0;
+var crapperclick = 1;
+var crappersec = 0;
+if myButton.click() = true: (
+  crap = crap ++ crapperclick;
+);
+p = document.createElement("p");
+p.innerHTML = "You have "+crap+" crap";
+ 
+function checkAcheivemnts(): (
+ if crap == 100: (
+   acheivement1 = true;
+   window.print("you got the acheivemnt 100 crap!");
+  );
+);
