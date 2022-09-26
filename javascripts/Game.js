6d7d0392317bb9a168bbe0baabba3225e8d92db5
Joe Musashi <63985175+JoeMusashi9999@users.noreply.github.com>
2022-09-25 19:58:38 -0700

    Update Game.js

--- javascripts/Game.js
+++ javascripts/Game.js
@@ -5,18 +5,18 @@
 
 var Player = {
   
-  acheivement1: new boolean(false);
-  acheivement2: new boolean(false);
-  acheivement3: new boolean(false);
-  acheivement4: new boolean(false);
-  acheivement5: new boolean(false);
-  acheivement6: new boolean(false);
-  acheivement7: new boolean(false);
-  acheivement8: new boolean(false);
-  acheivement9: new boolean(false);
-  crap: 0;
-  crapperclick: 1;
-  crappersec: 0;
+  acheivement1: false,
+  acheivement2: false,
+  acheivement3: false,
+  acheivement4: false,
+  acheivement5: false,
+  acheivement6: false,
+  acheivement7: false,
+  acheivement8: false,
+  acheivement9: false,
+  crap: 0,
+  crapperclick: 1,
+  crappersec: 0,
   options: {
         newsHidden: false,
         notation: "Mixed scientific",
@@ -25,17 +25,17 @@ var Player = {
 
 
 
-
+//update crap not working
 function updateCrap() {
-    var element = document.getElementById("CrapAmount");
-    element.textContent = formatValue(player.options.notation, player.crap, 2, 1);
-    document.getElementById("CrapAmount").innerHTML = player.crap;
+  document.getElementById("CrapAmount").textContent=(String(Player.crap+=1));
+
+
 }   
 
 
-function checkAcheivemnts(): (
- if crap == 100: (
-   acheivement1 = true;
-   window.print("you got the acheivemnt 100 crap!");
-  );
-);
+function checkAcheivemnts() {
+  if (Player.crap == 100) {
+    acheivement1 = true;
+    window.print("you got the acheivement 100 crap!");
+  }
+}
